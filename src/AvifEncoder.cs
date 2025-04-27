447d4d99838c5cc0a2afc90e06ebf46d3026cf12
Timo Witte <timo.witte@gmail.com>
2025-04-27 18:14:12 +0200

    add native library

--- src/AvifEncoder.cs
+++ src/AvifEncoder.cs
@@ -1,13 +1,11 @@
-using System.Reflection;
 using System.Runtime.InteropServices;
 using LibAvifSharp.NativeTypes;
 using SkiaSharp;
 
 namespace LibAvifSharp;
 
-public unsafe static partial class AvifEncoder
+public static class AvifEncoder
 {
-
     public static void Encode(SKBitmap bm, string outputPath, Action<EncoderSetttings>? settings = null)
     {
         var encoderSettings = new EncoderSetttings();
@@ -16,27 +14,27 @@ public unsafe static partial class AvifEncoder
             settings(encoderSettings);
         }
 
-        RegisterNativeLibraryLoader();
+        NativeInterop.EnsureNativeLibraryLoaderRegistered();
         var pixels = bm.GetPixels();
 
-        var image = avifImageCreate((uint)bm.Width, (uint)bm.Height, (uint)(bm.ColorType == SKColorType.Bgra8888 ? 8 : 16), encoderSettings.PixelFormat);
+        var image = NativeInterop.avifImageCreate((uint)bm.Width, (uint)bm.Height, (uint)(bm.ColorType == SKColorType.Bgra8888 ? 8 : 16), encoderSettings.PixelFormat);
     
 
         var rgb = new AvifRGBImage();
 
-        avifRGBImageSetDefaults(ref rgb, image);
+        NativeInterop.avifRGBImageSetDefaults(in rgb, image);
 
         rgb.Pixels = pixels;
         rgb.RowBytes = (uint)bm.RowBytes;
         rgb.Format = AvifRGBFormat.AVIF_RGB_FORMAT_BGRA;
 
-        var result = avifImageRGBToYUV(image, ref rgb);
+        var result = NativeInterop.avifImageRGBToYUV(image, in rgb);
         if(result != AvifResult.AVIF_RESULT_OK)
         {
             throw new Exception($"Failed to convert RGB to YUV: {result}");
         }
 
-        var encoderPtr = avifEncoderCreate();
+        var encoderPtr = NativeInterop.avifEncoderCreate();
 
         var encoder = Marshal.PtrToStructure<NativeTypes.AvifEncoder>(encoderPtr);
 
@@ -47,70 +45,23 @@ public unsafe static partial class AvifEncoder
 
         Marshal.StructureToPtr(encoder, encoderPtr, false);
 
-        AvifResult addImageResult = avifEncoderAddImage(encoderPtr, image, 1, AvifAddImageFlag.AVIF_ADD_IMAGE_FLAG_SINGLE);
+        AvifResult addImageResult = NativeInterop.avifEncoderAddImage(encoderPtr, image, 1, AvifAddImageFlag.AVIF_ADD_IMAGE_FLAG_SINGLE);
         if (addImageResult != AvifResult.AVIF_RESULT_OK)
         {
             throw new Exception($"Failed to add image: {addImageResult}");
         }
 
         AvifRWData output = new AvifRWData();
-        avifEncoderFinish(encoderPtr, ref output);
+        NativeInterop.avifEncoderFinish(encoderPtr, ref output);
+        NativeInterop.avifEncoderDestroy(encoderPtr);
+        NativeInterop.avifImageDestroy(image);
 
-        var data = new Span<byte>(output.Data.ToPointer(), (int)output.Size);
-
-        File.WriteAllBytes(outputPath, data);
-
-        avifEncoderDestroy(encoderPtr);
-    }
-
-    [LibraryImport("libavif")]
-    private static partial IntPtr avifImageCreate(uint width, uint height, uint depth, AvifPixelFormat yuvFormat);
-
-    [LibraryImport("libavif")]
-    private static partial IntPtr avifEncoderCreate();
-
-    [LibraryImport("libavif")]
-    private static partial AvifResult avifEncoderAddImage(IntPtr encoder, IntPtr image, ulong duration, AvifAddImageFlag flags);
-
-    [LibraryImport("libavif")]
-    private static partial  AvifResult avifEncoderFinish(IntPtr encoder, ref AvifRWData output);
-
-    [LibraryImport("libavif")]
-    private static partial int avifEncoderDestroy(IntPtr encoder);
-
-    [LibraryImport("libavif")]
-    private static partial void avifRGBImageSetDefaults(in AvifRGBImage rgb, IntPtr image);
-
-    [LibraryImport("libavif")]
-    private static partial AvifResult avifImageRGBToYUV(IntPtr image, in AvifRGBImage rgb);
-
-
-    private static object _lock = new object();
-    private static bool resolverSet = false;
-    private static void RegisterNativeLibraryLoader()
-    {
-        lock(_lock)
+        unsafe
         {
-            if(resolverSet)
-                return;
-
-            resolverSet = true;
-            NativeLibrary.SetDllImportResolver(Assembly.GetExecutingAssembly(), DllImportResolver);
-        }
-    }
-
-    private static IntPtr DllImportResolver(string libraryName, Assembly assembly, DllImportSearchPath? searchPath)
-    {
-        if (libraryName == "libavif")
-        {
-            // On systems with AVX2 support, load a different library.
-            if (System.Runtime.Intrinsics.X86.Avx2.IsSupported)
-            {
-                return NativeLibrary.Load("/home/spacy/src/libavif/build/libavif.so.16.2.1", assembly, searchPath);
-            }
+            var data = new Span<byte>(output.Data.ToPointer(), (int)output.Size);
+            File.WriteAllBytes(outputPath, data);
         }
 
-        // Otherwise, fallback to default import resolver.
-        return IntPtr.Zero;
+        NativeInterop.avifRWDataFree(ref output);
     }
 }
